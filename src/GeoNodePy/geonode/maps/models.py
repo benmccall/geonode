03e3535ba2b26081053c985e54b84a63bce5dbec
Ben McCall <ben@iogo.com>
2012-06-11 14:55:49 -0500

    Fix typo bug and check for nonexistent contacts on existing users created prior to fix

--- src/GeoNodePy/geonode/maps/models.py
+++ src/GeoNodePy/geonode/maps/models.py
@@ -517,8 +517,7 @@ class Contact(models.Model):
 
 
 def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Contact.objects.create(user=instance, 'name': instance.username )
+    profile, created = Contact.objects.get_or_create(user=instance, defaults={'name': instance.username})
 
 signals.post_save.connect(create_user_profile, sender=User)
 
